928d42069ca9db4e8301b03ebc1749f3e16cc7b1
georgesmhanna <georgesmhanna92@gmail.com>
2019-05-26 21:06:00 +0300

    Added register function in auth service Added @Input and @Output for component communication parent <> child

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AuthService} from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,17 +10,25 @@ export class RegisterComponent implements OnInit {
 
   private model: any = {};
 
-  constructor() {
+  @Input() valuesFromHome: any;
+  @Output() cancelRegister = new EventEmitter();
+
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit() {
   }
 
   register() {
-    console.log(this.model);
+    return this.authService.register(this.model).subscribe(() => {
+      console.log('user register success');
+    }, error => {
+      console.log('error on register, error = ', error);
+    });
   }
 
   cancel() {
+    this.cancelRegister.emit(false);
     console.log('canceled');
   }
 }
